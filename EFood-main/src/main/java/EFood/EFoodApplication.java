35f77c8c5c4515050c71a0730b583b491e3f2e9b
namizz <139370392+namizz@users.noreply.github.com>
2025-03-28 11:04:58 +0300

    backend

--- EFood-main/src/main/java/EFood/EFoodApplication.java
+++ EFood-main/src/main/java/EFood/EFoodApplication.java
@@ -10,25 +10,25 @@ public class EFoodApplication {
 
 	public static void main(String[] args) {
 		// Load the .env file
-		Dotenv dotenv = Dotenv.load();
+		// Dotenv dotenv = Dotenv.load();
 
 		// Retrieve credentials from the .env file for hosting
-		// String dbUrl = System.getenv("DB_URL");
-		// String dbUsername = System.getenv("DB_USERNAME");
-		// String dbPassword = System.getenv("DB_PASSWORD");
-		// String jwt_secret_key = System.getenv("JWT_SECRET_KEY");
-		// String cloud_name = System.getenv("CLOUD_NAME");
-		// String cloud_api_key = System.getenv("CLOUD_API_KEY");
-		// String cloud_api_secret = System.getenv("CLOUD_API_SECRET");
+		String dbUrl = System.getenv("DB_URL");
+		String dbUsername = System.getenv("DB_USERNAME");
+		String dbPassword = System.getenv("DB_PASSWORD");
+		String jwt_secret_key = System.getenv("JWT_SECRET_KEY");
+		String cloud_name = System.getenv("CLOUD_NAME");
+		String cloud_api_key = System.getenv("CLOUD_API_KEY");
+		String cloud_api_secret = System.getenv("CLOUD_API_SECRET");
 
 		// // for checking in local host
-		String dbUrl = dotenv.get("DB_URL");
-		String dbUsername = dotenv.get("DB_USERNAME");
-		String dbPassword = dotenv.get("DB_PASSWORD");
-		String jwt_secret_key = dotenv.get("JWT_SECRET_KEY");
-		String cloud_name = dotenv.get("CLOUD_NAME");
-		String cloud_api_key = dotenv.get("CLOUD_API_KEY");
-		String cloud_api_secret = dotenv.get("CLOUD_API_SECRET");
+		// String dbUrl = dotenv.get("DB_URL");
+		// String dbUsername = dotenv.get("DB_USERNAME");
+		// String dbPassword = dotenv.get("DB_PASSWORD");
+		// String jwt_secret_key = dotenv.get("JWT_SECRET_KEY");
+		// String cloud_name = dotenv.get("CLOUD_NAME");
+		// String cloud_api_key = dotenv.get("CLOUD_API_KEY");
+		// String cloud_api_secret = dotenv.get("CLOUD_API_SECRET");
 
 		// Pass them to Spring Boot's datasource configuration
 		System.setProperty("datasource.url", dbUrl);
